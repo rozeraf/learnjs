21dee723500165191e05e572eba7ae1f07d24788
Раф <rafabduloff@gmail.com>
2025-10-01 22:22:47 +0500

    feat(lesson3): add sum calculation functionality
    - Implement manual sum calculation using for loop
    - Create reusable sumArray function with type annotations
    - Add console logs to demonstrate functionality

--- lesson3/lesson3.ts
+++ lesson3/lesson3.ts
@@ -8,3 +8,22 @@ const names: string[] = ["иван", "раф", "таир"]
 names.forEach((name) => {
     console.log(`Привет, ${name}`)
 });
+// повтор твоего кода
+const numbers2: number[] = [10, 20, 30];
+let sum = 0; // вероятно затем, чтобы изначальный результат будет 0, после чего будет прибавляться?
+for (let i = 0; i < numbers2.length;
+    i++) {
+        // i - это индекс (0,1,2) - ладно, допустим.
+        // numbers[i] - это элемент, например 10, 20 или 30. - допустим.
+        sum = sum + numbers2[i];
+    }
+console.log(sum)
+// задача 3
+const sumArray = (data: number[]): number => {
+    let sum = 0;
+    for (let i = 0; i < data.length; i++) {
+        sum = sum + data[i];
+    }
+    return sum;
+}
+console.log(sumArray(numbers))
