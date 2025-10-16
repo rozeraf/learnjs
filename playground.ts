2ac4631b5eefb7981d6097597d20463bc54d325a
Раф <rafabduloff@gmail.com>
2025-10-16 22:34:08 +0500

    feat(playground): add Post interface and update logic
    - Introduce Post interface to define post structure
    - Implement PostUpdateDto for updating posts with validation
    - Create PostCard type to extract specific fields from Post
    - Define NewPostDto to omit unnecessary fields for new posts
    - Add Readonly type for post object to ensure immutability
    - Introduce PostCache type for caching posts by ID

--- playground.ts
+++ playground.ts
@@ -1,20 +1,34 @@
-type Counter = {
-  increment: () => number;
-  decrement: () => number;
-  reset: () => number;
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  authorId: number;
+  createdAt?: Date; // чтобы ts не ргулася на то что в post нету его
+}
+// q1
+// раз не было условия что пользователь может прислать пустоту, значит такое допускать нельзя. сделаем проверку.
+// я могу написать на partial, если надо, но это нехорошо со стороны разработчика, он может пустой объект передат
+type PostUpdateDto =
+  | { title: string; body?: string }
+  | { title?: string; body: string };
+const updatePost = (data: PostUpdateDto) => {
+  if (!data.title && !data.body) {
+    throw new Error('you need to pass at least one field');
+  }
+  // update logic
+};
+// q2
+type PostCard = Pick<Post, 'id' | 'title' | 'authorId'>;
+
+// q3
+type NewPostDto = Omit<Post, 'id' | 'createdAt'>;
+
+// q4
+const post: Readonly<Post> = {
+  id: 0,
+  title: '',
+  body: '',
+  authorId: 0,
 };
 
-function createCounter(init: number): Counter {
-  let current = init;
-  return {
-    increment(): number {
-      return ++current;
-    },
-    decrement(): number {
-      return --current;
-    },
-    reset(): number {
-      return (current = init);
-    },
-  };
-}
+type PostCache = Record<string, Post>;
